738ed28d36ba126b1e1a044cbb8623ec49aa775d
Riccardo Pucella <riccardo.pucella@gmail.com>
2020-08-29 11:03:48 -0400

    Fix navbar so that hamburger menu works

--- react/src/App.js
+++ react/src/App.js
@@ -43,15 +43,18 @@ const Error = (screen) => () => (
 
 const Header = () => {
   const navigateTo = useContext(NavigationContext)
+  const [ showMenu, setShowMenu ] = useState(false)
+  const toggleMenu = () => setShowMenu(!showMenu)
+  const goTo = (dest) => () => { setShowMenu(false); navigateTo(dest) }
   return (
-  <nav className="navbar is-tablet" role="navigation" aria-label="main navigation">
+    <nav className="navbar is-tablet" role="navigation" aria-label="main navigation">
 
       <div className="navbar-brand">
-        <Link className="navbar-item" onClick={() => navigateTo('published')}>
+        <Link className="navbar-item" onClick={goTo('published')}>
           <Logo>ImageBank</Logo>
         </Link>
 
-        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <a role="button" className={showMenu ? "navbar-burger is-active" : "navbar-burger"} aria-label="menu" aria-expanded="false" data-target="navbarMenu" onClick={toggleMenu}>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -59,14 +62,14 @@ const Header = () => {
         
       </div>
       
-      <div id="navbarBasicExample" className="navbar-menu">
+      <div id="navbarMenu" className={showMenu ? "navbar-menu is-active" : "navbar-menu"}>
         <div className="navbar-start">
-          <Link onClick={() => navigateTo('published')} className="navbar-item"> Published </Link>
-          <Link onClick={() => navigateTo('draft')} className="navbar-item"> Drafts </Link>
-          <Link onClick={() => navigateTo('new')} className="navbar-item"> New </Link>
-          <Link onClick={() => navigateTo('tags')} className="navbar-item"> Tags </Link>
-          { /* <Link onClick={() => navigateTo('note')} className="navbar-item"> Notes </Link> */ }
-          <Link onClick={() => navigateTo('add')} className="navbar-item"> <span><img src={addSvg} height="12" width="12" /> Image</span> </Link>
+          <Link onClick={goTo('published')} className="navbar-item"> Published </Link>
+          <Link onClick={goTo('draft')} className="navbar-item"> Drafts </Link>
+          <Link onClick={goTo('new')} className="navbar-item"> New </Link>
+          <Link onClick={goTo('tags')} className="navbar-item"> Tags </Link>
+          { /* <Link onClick={goTo('note')} className="navbar-item"> Notes </Link> */ }
+          <Link onClick={goTo('add')} className="navbar-item"> <span><img src={addSvg} height="12" width="12" /> Image</span> </Link>
         </div>
       </div>
     </nav>
